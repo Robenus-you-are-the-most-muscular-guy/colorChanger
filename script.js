1364d0b30d7e83ab31e08b3a562bc44205c50ef0
Bestest Coder <85369047+Robenus-you-are-the-most-muscular-guy@users.noreply.github.com>
2022-05-01 22:35:12 +0000

    color to colors variable name

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const button = document.querySelector('button')
 const body = document.querySelector('body')
-const color = ['red', 'green', 'orange', 'blue', 'purple']
+const colors = ['red', 'green', 'orange', 'blue', 'purple']
 
 document.body.style.backgroundColor = 'gray'
 button.addEventListener('click', changerBG)
 
 function changerBG() {
-  const colorIndex = parseInt(Math.random() * color.length)
-  document.body.style.backgroundColor = color[colorIndex]
+  const colorIndex = parseInt(Math.random() * colors.length)
+  document.body.style.backgroundColor = colors[colorIndex]
 }
